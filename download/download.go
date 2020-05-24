49652cfde2cd70fb5bbb86771e91101daa7381ed
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-05-23 22:46:51 -0400

    Check that the server supports range requests

--- download/download.go
+++ download/download.go
@@ -1,6 +1,7 @@
 package download
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -10,20 +11,26 @@ import (
 )
 
 func Run(url string) error {
-	contentLength, err := contentLength(url)
+	res, err := http.Head(url)
 	if err != nil {
-		return err
+		return fmt.Errorf("error making HEAD request: %w", err)
 	}
+
+	if !supportsRangeRequests(res) {
+		return errors.New("server does not support range requests")
+	}
+
+	contentLength := res.ContentLength
 
 	filename := filename(url)
 
 	log.Printf("Downloading %s (size %d)\n", filename, contentLength)
 
-	resp, err := http.Get(url)
+	res, err = http.Get(url)
 	if err != nil {
 		return fmt.Errorf("error making request: %w", err)
 	}
-	defer resp.Body.Close()
+	defer res.Body.Close()
 
 	out, err := os.Create(filename)
 	if err != nil {
@@ -31,7 +38,7 @@ func Run(url string) error {
 	}
 	defer out.Close()
 
-	written, err := io.Copy(out, resp.Body)
+	written, err := io.Copy(out, res.Body)
 	if err != nil {
 		return fmt.Errorf("eror writing file: %w", err)
 	}
@@ -41,16 +48,12 @@ func Run(url string) error {
 	return nil
 }
 
+func supportsRangeRequests(res *http.Response) bool {
+	acceptRanges := res.Header.Get("Accept-Ranges")
+	return strings.Contains(acceptRanges, "bytes")
+}
+
 func filename(url string) string {
 	segments := strings.Split(url, "/")
 	return segments[len(segments)-1]
-}
-
-func contentLength(url string) (int64, error) {
-	resp, err := http.Head(url)
-	if err != nil {
-		return 0, fmt.Errorf("error making HEAD request: %w", err)
-	}
-
-	return resp.ContentLength, nil
 }
