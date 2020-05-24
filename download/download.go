4e5078be1e20d1d3a789d4d00d0cd869204a4047
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-05-23 22:25:59 -0400

    Wrap errors and add details

--- download/download.go
+++ download/download.go
@@ -1,6 +1,7 @@
 package download
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -15,19 +16,19 @@ func Run(url string) error {
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		return fmt.Errorf("error making request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	out, err := os.Create(filename)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating file: %w", err)
 	}
 	defer out.Close()
 
 	written, err := io.Copy(out, resp.Body)
 	if err != nil {
-		return err
+		return fmt.Errorf("eror writing file: %w", err)
 	}
 
 	log.Printf("Wrote %d bytes\n", written)
