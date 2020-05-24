5509bcb3a1c9cbd2c13b4167adb4c8602d4253e3
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-05-24 18:46:08 -0400

    Add baseline benchmark

--- download/download_test.go
+++ download/download_test.go
@@ -8,6 +8,12 @@ import (
 
 func BenchmarkDownload(b *testing.B) {
 	b.Run("thinkbroadband 1GB", func(b *testing.B) {
+		b.Run("1 chunk", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, "http://ipv4.download.thinkbroadband.com/1GB.zip", 1)
+			}
+		})
+
 		b.Run("10 chunks", func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				downloadChunked(b, "http://ipv4.download.thinkbroadband.com/1GB.zip", 10)
