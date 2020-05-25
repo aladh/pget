69c1e4b09fe4806193c21d9b11cc38d201116153
Alishan Ladhani <alishan.ladhani@gmail.com>
2020-05-24 23:35:46 -0400

    Add hetzner benchmarks

--- download/download_test.go
+++ download/download_test.go
@@ -40,6 +40,40 @@ func BenchmarkDownload(b *testing.B) {
 			}
 		})
 	})
+
+	b.Run("hetzner 1GB", func(b *testing.B) {
+		url := "https://speed.hetzner.de/1GB.bin"
+
+		b.Run("1 chunk", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, url, 1)
+			}
+		})
+
+		b.Run("10 chunks", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, url, 10)
+			}
+		})
+
+		b.Run("20 chunks", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, url, 20)
+			}
+		})
+
+		b.Run("40 chunks", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, url, 40)
+			}
+		})
+
+		b.Run("80 chunks", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				downloadChunked(b, url, 80)
+			}
+		})
+	})
 }
 
 func downloadChunked(b *testing.B, url string, numChunks int) {
